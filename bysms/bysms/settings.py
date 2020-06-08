85da7fbea0a0c0d5db0f99682a8bc598aabc9c4e
wxhyxy <w.3097652.rt@163.com>
2020-06-08 09:21:17 +0800

    添加数据库

--- bysms/bysms/settings.py
+++ bysms/bysms/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bysms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'mysql'
     }
 }
 
