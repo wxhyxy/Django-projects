4d78d3b1cd25ce24ebda0d2a22dcfdcc0d74f83a
wxhyxy <w.3097652.rt@163.com>
2020-09-13 16:59:19 +0800

    order

--- bysms/mgr/order.py
+++ bysms/mgr/order.py
@@ -7,6 +7,8 @@ from django.db import IntegrityError, transaction
 
 from common.models import Order, OrderMedicine
 
+from django.db.models import F
+
 import json
 
 
@@ -24,7 +26,7 @@ def dispatcher(request):
             'ret': 302,
             'msg': '用户非mgr类型',
             'redirect': '/mgr/sign.html'
-        },status=302)
+        }, status=302)
 
     # 将请求参数统一放入request的params属性中，方便后续处理
     # GET请求参数在request对象的GET属性中
@@ -49,5 +51,45 @@ def dispatcher(request):
             'msg': '不支持该类型http请求'
         })
 
+
 def addorder(request):
     info = request.params['data']
+
+    # 从请求消息中 获取要添加订单信息
+    # 并且插入到数据库
+    # Django 实现数据库事务操作
+    with transaction.atomic():
+        new_order = Order.objects.create(name=info['name'], customer_id=info['customerid'])
+
+        batch = [OrderMedicine(order_id=new_order.id, medicine_id=mid, amount=1)
+                 for mid in info['medicineids']]
+        OrderMedicine.objects.bulk_create(batch)
+
+    return JsonResponse({'ret': 0, 'id': new_order.id})
+
+
+def listorder(request):
+    qs = Order.objects \
+        .annotate(
+        customer_name=F('customer__name'),
+        medicines_name=F('medicines__name')
+    ) \
+        .values(
+        'id', 'name', 'create_date', 'customer_name', 'medicines_name'
+    )
+
+    # 将QuerySet对象转化为list类型
+    retlist = list(qs)
+
+    # 可能有ID 相同，药品不同的订单记录，需要合并
+    newlist = []
+    id2order = {}
+    for one in retlist:
+        orderid = one['id']
+        if orderid not in id2order:
+            newlist.append(one)
+            id2order[orderid] = one
+        else:
+            id2order[orderid]['medicines_name'] += ' | ' + one['medicines_name']
+
+        return JsonResponse({'ret':0, 'retlist': newlist})
